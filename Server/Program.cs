3939a3285b9a5fa5bdd9aa577febd631ba199a27
Ivan Kaliakin <vovano2006@yandex.ru>
2026-06-04 01:32:35 +0200

    Removing Heroku-specific proxy config

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,24 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
-var isHeroku = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO"));
-if (isHeroku)
-{
-    builder.Services.Configure<Microsoft.AspNetCore.HttpOverrides.ForwardedHeadersOptions>(options =>
-    {
-        options.ForwardedHeaders =
-            Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor |
-            Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto;
-        options.KnownIPNetworks.Clear();
-        options.KnownProxies.Clear();
-    });
-}
-
 var app = builder.Build();
-
-if (isHeroku)
-{
-    app.UseForwardedHeaders();
-}
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
